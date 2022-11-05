ce626fb653780af93c719eab042cf54bc66a755c
kumresh-soy <kumresh030996@gmail.com>
2022-11-05 23:56:59 +0530

    changing APIView to viewset

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,17 @@
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet,CreateUser,UserProfileView,CustomAuthToken, UserFollow
+from .views import UserViewSet, CreateUser, UserProfileView, CustomAuthToken, UserFollow
+
 router = routers.DefaultRouter()
 
-router.register('users', UserViewSet)
+router.register('users', UserViewSet, basename="users")
+router.register("create-user", CreateUser, basename="create-user")
+router.register("profile", UserProfileView, basename="profile")
 urlpatterns = [
     path('', include(router.urls)),
-    path('create-user/', CreateUser.as_view(), name='create-user'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
+    # path('create-user/', CreateUser.as_view(), name='create-user'),
+    # path('profile/', UserProfileView.as_view(), name='profile'),
     path('login/', CustomAuthToken.as_view(), name="login"),
     path('follow/<int:pk>/', UserFollow.as_view(), name='user-follow'),
 ]
